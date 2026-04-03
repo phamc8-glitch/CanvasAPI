ed60f70e8ccc7e79fd670eeef411aba7fc1f3620
phamc8-glitch <phamc8@vcu.edu>
2026-04-02 23:13:13 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,196 @@
+//Get Things
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+
+//reds
+ctx.fillStyle = "red";
+ctx.fillRect(0, 0, 1920, 5); 
+
+ctx.fillStyle = "red";
+ctx.fillRect(0, 10, 1920, 5); 
+
+ctx.fillStyle = "red";
+ctx.fillRect(0, 20, 1920, 5);
+
+ctx.fillStyle = "red";
+ctx.fillRect(0, 30, 1920, 5);
+
+ctx.fillStyle = "red";
+ctx.fillRect(0, 40, 1920, 5); 
+
+ctx.fillStyle = "red";
+ctx.fillRect(0, 50, 1920, 5);
+
+ctx.fillStyle = "red";
+ctx.fillRect(0, 75, 1920, 5); 
+
+ctx.fillStyle = "red";
+ctx.fillRect(0, 100, 1920, 5);
+
+//magenta
+ctx.fillStyle = "magenta";
+ctx.fillRect(0, -50+250, 1920, 5); 
+
+ctx.fillStyle = "magenta";
+ctx.fillRect(0, -25+250, 1920, 5); 
+
+ctx.fillStyle = "magenta";
+ctx.fillRect(0, 0+250, 1920, 5); 
+
+ctx.fillStyle = "magenta";
+ctx.fillRect(0, 10+250, 1920, 5); 
+
+ctx.fillStyle = "magenta";
+ctx.fillRect(0, 20+250, 1920, 5);
+
+ctx.fillStyle = "magenta";
+ctx.fillRect(0, 30+250, 1920, 5);
+
+ctx.fillStyle = "magenta";
+ctx.fillRect(0, 40+250, 1920, 5); 
+
+ctx.fillStyle = "magenta";
+ctx.fillRect(0, 50+250, 1920, 5);
+
+ctx.fillStyle = "magenta";
+ctx.fillRect(0, 75+250, 1920, 5); 
+
+ctx.fillStyle = "magenta";
+ctx.fillRect(0, 100+250, 1920, 5);
+
+//reds
+ctx.fillStyle = "red";
+ctx.fillRect(0, 0, 1920, 5); 
+
+ctx.fillStyle = "red";
+ctx.fillRect(0, 10, 1920, 5); 
+
+ctx.fillStyle = "red";
+ctx.fillRect(0, 20, 1920, 5);
+
+ctx.fillStyle = "red";
+ctx.fillRect(0, 30, 1920, 5);
+
+ctx.fillStyle = "red";
+ctx.fillRect(0, 40, 1920, 5); 
+
+ctx.fillStyle = "red";
+ctx.fillRect(0, 50, 1920, 5);
+
+ctx.fillStyle = "red";
+ctx.fillRect(0, 75, 1920, 5); 
+
+ctx.fillStyle = "red";
+ctx.fillRect(0, 100, 1920, 5);
+
+//blue
+ctx.fillStyle = "blue";
+ctx.fillRect(0, -50+500, 1920, 5); 
+
+ctx.fillStyle = "blue";
+ctx.fillRect(0, -25+500, 1920, 5); 
+
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 0+500, 1920, 5); 
+
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 10+500, 1920, 5); 
+
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 20+500, 1920, 5);
+
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 30+500, 1920, 5);
+
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 40+500, 1920, 5); 
+
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 50+500, 1920, 5);
+
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 75+500, 1920, 5); 
+
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 100+500, 1920, 5);
+
+//cyan
+ctx.fillStyle = "cyan";
+ctx.fillRect(0, -50+750, 1920, 5); 
+
+ctx.fillStyle = "cyan";
+ctx.fillRect(0, -25+750, 1920, 5); 
+
+ctx.fillStyle = "cyan";
+ctx.fillRect(0, 0+750, 1920, 5); 
+
+ctx.fillStyle = "cyan";
+ctx.fillRect(0, 10+750, 1920, 5); 
+
+ctx.fillStyle = "cyan";
+ctx.fillRect(0, 20+750, 1920, 5);
+
+ctx.fillStyle = "cyan";
+ctx.fillRect(0, 30+750, 1920, 5);
+
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 40+500, 1920, 5); 
+
+ctx.fillStyle = "cyan";
+ctx.fillRect(0, 50+750, 1920, 5);
+
+ctx.fillStyle = "cyan";
+ctx.fillRect(0, 75+750, 1920, 5); 
+
+ctx.fillStyle = "cyan";
+ctx.fillRect(0, 100+750, 1920, 5);
+
+//blue vertical
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 0, 5, 1080);  
+
+ctx.fillStyle = "blue";
+ctx.fillRect(10, 0, 5, 1080); 
+
+ctx.fillStyle = "blue";
+ctx.fillRect(20, 0, 5, 1080); 
+
+ctx.fillStyle = "blue";
+ctx.fillRect(30, 0, 5, 1080); 
+
+ctx.fillStyle = "blue";
+ctx.fillRect(40, 0, 5, 1080); 
+
+ctx.fillStyle = "blue";
+ctx.fillRect(50, 0, 5, 1080); 
+
+ctx.fillStyle = "blue";
+ctx.fillRect(75, 0, 5, 1080);
+
+ctx.fillStyle = "blue";
+ctx.fillRect(100, 0, 5, 1080);
+
+//blue vertical
+ctx.fillStyle = "blue";
+ctx.fillRect(1912-100, 0, 5, 1080);  
+
+ctx.fillStyle = "blue";
+ctx.fillRect(1915-75, 0, 5, 1080);  
+
+ctx.fillStyle = "blue";
+ctx.fillRect(1915-50, 0, 5, 1080);  
+
+ctx.fillStyle = "blue";
+ctx.fillRect(1915-40, 0, 5, 1080); 
+
+ctx.fillStyle = "blue";
+ctx.fillRect(1915-30, 0, 5, 1080); 
+
+ctx.fillStyle = "blue";
+ctx.fillRect(1915-20, 0, 5, 1080); 
+
+ctx.fillStyle = "blue";
+ctx.fillRect(1915-10, 0, 5, 1080); 
+
+ctx.fillStyle = "blue";
+ctx.fillRect(1915, 0, 5, 1080); 
